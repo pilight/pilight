fd845a1b7fc1bb5c6c9734f283eb0c7944b508cd
CurlyMoo <curlymoo1@gmail.com>
2013-07-19 23:50:49 +0200

    Update arctech_switch.c
    Possible fix to disable the requirement of the `unit` argument when also using `all`.

--- protocols/arctech_switch.c
+++ protocols/arctech_switch.c
@@ -141,13 +141,13 @@ int arctechSwCreateCode(JsonNode *code) {
 	if(json_find_string(code, "all", &tmp) == 0)
 		all = 1;
 
-	if(id == -1 || unit == -1 || state == -1) {
+	if(id == -1 || (unit == -1 && all == 0) || state == -1) {
 		logprintf(LOG_ERR, "arctech_switch: insufficient number of arguments");
 		return EXIT_FAILURE;
 	} else if(id > 67108863 || id < 1) {
 		logprintf(LOG_ERR, "arctech_switch: invalid id range");
 		return EXIT_FAILURE;
-	} else if(unit > 16 || unit < 0) {
+	} else if((unit > 16 || unit < 0) && all == 0) {
 		logprintf(LOG_ERR, "arctech_switch: invalid unit range");
 		return EXIT_FAILURE;
 	} else {
