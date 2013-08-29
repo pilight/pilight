93bd876d593a725742d3c4d1cad0ab2e6617c939
CurlyMoo <curlymoo1@gmail.com>
2013-08-29 20:24:34 +0200

    Allow up to 2 decimal temperatures in webgui

--- web/pilight.js
+++ web/pilight.js
@@ -52,6 +52,12 @@ function createDimmerElement(sTabId, sDevId, sDevName, sDevProto, sState, iDimLe
 function createWeatherElement(sTabId, sDevId, sDevName, sDevProto, iTemperature, iHumidity, iBattery) {
 	oTab = $('#'+sTabId).find('ul');
 	oTab.append($('<li data-icon="false">'+sDevName+'<div class="temperature" id="'+sTabId+'_'+sDevId+'_temp">'+(iTemperature/100)+'</div><div class="degrees">o</div><div class="humidity" id="'+sTabId+'_'+sDevId+'_humi">'+iHumidity+'</div><div class="percentage">%</div></li>'));
+	if(iTemperature > 100) {
+		iTemperature /= 10;
+	}	
+	else if(iTemperature > 1000) {
+		iTemperature /= 100;
+	}
 	if(sDevProto == "alecto") {
 		oTab.find('li').append($('<div id="'+sTabId+'_'+sDevId+'_batt" class="battery"></div>'));
 	}
