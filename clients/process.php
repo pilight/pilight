9aea866429e3fa9fbec586ff72ba1511f54afecb
CurlyMoo <curlymoo1@gmail.com>
2013-12-10 23:07:27 +0100

    Added GPLv3 headers to clients

--- clients/process.php
+++ clients/process.php
@@ -1,5 +1,23 @@
 #!/usr/bin/env php
 <?php
+/*
+	Copyright (C) 2013 CurlyMo
+
+	This file is part of pilight.
+
+    pilight is free software: you can redistribute it and/or modify it under the
+	terms of the GNU General Public License as published by the Free Software
+	Foundation, either version 3 of the License, or (at your option) any later
+	version.
+
+    pilight is distributed in the hope that it will be useful, but WITHOUT ANY
+	WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+	A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pilight. If not, see	<http://www.gnu.org/licenses/>
+*/
+
 $sMsg = "M-SEARCH * HTTP/1.1\r\n";
 $sMsg .= "Host:239.255.255.250:1900\r\n";
 $sMsg .= "ST:urn:schemas-upnp-org:service:pilight:1\r\n";
