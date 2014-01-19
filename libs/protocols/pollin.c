9737b42fdf6a130bdda8ee4f82ce7f8fffe72175
lcd4linux <tako-ist@web.de>
2014-01-19 16:23:32 +0100

    Update pollin.c

--- libs/protocols/pollin.c
+++ libs/protocols/pollin.c
@@ -33,7 +33,7 @@ void pollinCreateMessage(int systemcode, int unitcode, int state) {
 	pollin->message = json_mkobject();
 	json_append_member(pollin->message, "systemcode", json_mknumber(systemcode));
 	json_append_member(pollin->message, "unitcode", json_mknumber(unitcode));
-	if(state == 1) {
+	if(state == 0) {
 		json_append_member(pollin->message, "state", json_mkstring("on"));
 	} else {
 		json_append_member(pollin->message, "state", json_mkstring("off"));
@@ -101,7 +101,7 @@ void pollinCreateUnitCode(int unitcode) {
 }
 
 void pollinCreateState(int state) {
-	if(state == 1) {
+	if(state == 0) {
 		pollinCreateHigh(44, 47);
 	}
 }
