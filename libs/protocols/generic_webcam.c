4fd694b70eba316043f30c1a5aedd4587220a0fe
1000io <1000io.com@gmail.com>
2014-01-31 09:58:17 +0100

    Update generic_webcam.c
    Using wunderground as base

--- libs/protocols/generic_webcam.c
+++ libs/protocols/generic_webcam.c
@@ -18,77 +18,252 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 #include <string.h>
+#include <unistd.h>
+#define __USE_GNU
+#include <pthread.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/time.h>
 
 #include "../../pilight.h"
 #include "common.h"
 #include "log.h"
+#include "threads.h"
+#include "http_lib.h"
 #include "protocol.h"
 #include "hardware.h"
 #include "binary.h"
+#include "json.h"
 #include "gc.h"
 #include "generic_webcam.h"
 
-//void genWebcamCreateMessage(int id, char *url, int refresh) {
-void genWebcamCreateMessage(int id, int refresh) {
-	generic_webcam->message = json_mkobject();
-	json_append_member(generic_webcam->message, "id", json_mknumber(id));
-	//json_append_member(generic_webcam->message, "url", json_mkstring(url));
-	if(refresh > -999) {
-		json_append_member(generic_webcam->message, "refresh", json_mknumber(refresh));
-	}
-}
-
-int genWebcamCreateCode(JsonNode *code) {
-	int id = -999;
+typedef struct generic_webcam_data_t {
 	char *url;
-	int refresh = -999;
-	char *tmp;
+	struct generic_webcam_data_t *next;
+} generic_webcam_data_t;
 
-	if(json_find_string(code, "id", &tmp) == 0)
-		id=atoi(tmp);
-	/*
-	if(json_find_string(code, "url", &tmp) == "")
-		url = atoi(tmp);
-	*/
-	
-	//&url = "";
-	
-	if(json_find_string(code, "refresh", &tmp) == 0)
-		refresh = atoi(tmp);
+unsigned short generic_webcam_loop = 1;
+unsigned short generic_webcam_nrfree = 0;
 
-	//if(id == -999 && url == "" && refresh == -999) {
-	if(id == -999 && refresh == -999) {
-		logprintf(LOG_ERR, "generic_webcam: insufficient number of arguments");
-		return EXIT_FAILURE;
-	} else {
-		//genWebcamCreateMessage(id, url, refresh);
-		genWebcamCreateMessage(id, refresh);
+void generic_webcamParseCleanUp(void *arg) {
+	struct generic_webcam_data_t *generic_webcam_data = (struct generic_webcam_data_t *)arg;
+	struct generic_webcam_data_t *wtmp = NULL;
+
+	generic_webcam_loop = 0;
+
+	while(generic_webcam_data) {
+		wtmp = generic_webcam_data;
+		sfree((void *)&generic_webcam_data->url);		
+		generic_webcam_data = generic_webcam_data->next;
+		sfree((void *)&wtmp);
 	}
-	return EXIT_SUCCESS;
+	sfree((void *)&generic_webcam_data);
 }
 
-void genWebcamPrintHelp(void) {
-	printf("\t -u --url=url\tset the camera url\n");
-	printf("\t -r --refresh=refresh\t\tset the camera refresh\n");
-	printf("\t -i --id=id\t\t\tcontrol a device with this id\n");
+void *generic_webcamParse(void *param) {
+
+/*	
+	struct JsonNode *json = (struct JsonNode *)param;
+	struct JsonNode *jid = NULL;
+	struct JsonNode *jchild = NULL;
+	struct JsonNode *jchild1 = NULL;
+	struct JsonNode *jsettings = NULL;
+	struct JsonNode *node = NULL;
+	struct generic_webcam_data_t *generic_webcam_data = NULL;
+	struct generic_webcam_data_t *wtmp = NULL;
+	struct timeval tp;
+	struct timespec ts;		
+	int interval = 900;
+
+	char url[1024];
+	char *filename = NULL, *data = NULL;
+	char typebuf[70];
+	char *stmp = NULL;
+	double temp = 0;
+	int humi = 0, rc = 0, lg = 0, ret = 0;
+	int firstrun = 1;	
+	JsonNode *jdata = NULL;
+	JsonNode *jobs = NULL;	
+
+#ifndef __FreeBSD__
+	pthread_mutex_t mutex = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;        
+    pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+#else
+	pthread_mutex_t mutex;
+	pthread_cond_t cond;
+	pthread_mutexattr_t attr;
+
+	pthread_mutexattr_init(&attr);
+	pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+	pthread_mutex_init(&mutex, &attr);
+#endif
+
+    pthread_cond_init(&cond, NULL);	
+
+	int has_country = 0, has_api = 0, has_location = 0;
+	if((jid = json_find_member(json, "id"))) {
+		jchild = json_first_child(jid);
+		while(jchild) {
+			has_country = 0, has_api = 0, has_location = 0;
+			jchild1 = json_first_child(jchild);
+			struct generic_webcam_data_t* wnode = malloc(sizeof(struct generic_webcam_data_t));
+			while(jchild1) {
+				if(strcmp(jchild1->key, "api") == 0) {
+					has_api = 1;
+					wnode->api = malloc(strlen(jchild1->string_)+1);
+					strcpy(wnode->api, jchild1->string_);
+				}
+				if(strcmp(jchild1->key, "location") == 0) {
+					has_location = 1;
+					wnode->location = malloc(strlen(jchild1->string_)+1);
+					strcpy(wnode->location, jchild1->string_);
+				}
+				if(strcmp(jchild1->key, "country") == 0) {
+					has_country = 1;
+					wnode->country = malloc(strlen(jchild1->string_)+1);
+					strcpy(wnode->country, jchild1->string_);
+				}
+				jchild1 = jchild1->next;
+			}
+			if(has_country && has_api && has_location) {
+				wnode->next = generic_webcam_data;
+				generic_webcam_data = wnode;
+			} else {
+				sfree((void *)&wnode);
+			}
+			jchild = jchild->next;
+		}
+	}
+	if((jsettings = json_find_member(json, "settings"))) {
+		json_find_number(jsettings, "interval", &interval);
+	}
+	json_delete(json);
+
+	pthread_cleanup_push(generic_webcamParseCleanUp, (void *)generic_webcam_data);
+
+	while(generic_webcam_loop) {
+		wtmp = generic_webcam_data;
+		rc = gettimeofday(&tp, NULL);
+		ts.tv_sec = tp.tv_sec;
+		ts.tv_nsec = tp.tv_usec * 1000;
+		if(firstrun) {
+			ts.tv_sec += 1;
+			firstrun = 0;
+		} else {
+			ts.tv_sec += interval;
+		}
+
+		pthread_mutex_lock(&mutex);
+		rc = pthread_cond_timedwait(&cond, &mutex, &ts);
+		if(rc == ETIMEDOUT) {
+			while(wtmp) {
+				filename = NULL;
+				data = NULL;
+				sprintf(url, "http://api.generic_webcam.com/api/%s/geolookup/conditions/q/%s/%s.json", wtmp->api, wtmp->country, wtmp->location);	
+				http_parse_url(url, &filename);
+				ret = http_get(filename, &data, &lg, typebuf);
+
+				if(ret == 200) {
+					if(strcmp(typebuf, "application/json;") == 0) {
+						if(json_validate(data) == true) {
+							if((jdata = json_decode(data)) != NULL) {
+								if((jobs = json_find_member(jdata, "current_observation")) != NULL) {
+									if((node = json_find_member(jobs, "temp_c")) == NULL) {
+										printf("api.generic_webcam.com json has no temp_c key");
+									} else if(json_find_string(jobs, "relative_humidity", &stmp) != 0) {
+										printf("api.generic_webcam.com json has no relative_humidity key");
+									} else {
+										if(node->tag != JSON_NUMBER) {
+											printf("api.generic_webcam.com json has no temp_c key");
+										} else {
+											temp = node->number_;
+											sscanf(stmp, "%d%%", &humi);
+											generic_webcam->message = json_mkobject();
+
+											JsonNode *code = json_mkobject();
+
+											json_append_member(code, "api", json_mkstring(wtmp->api));
+											json_append_member(code, "location", json_mkstring(wtmp->location));
+											json_append_member(code, "country", json_mkstring(wtmp->country));
+											json_append_member(code, "temperature", json_mknumber((int)(temp*100)));
+											json_append_member(code, "humidity", json_mknumber((int)(humi*100)));
+
+											json_append_member(generic_webcam->message, "code", code);
+											json_append_member(generic_webcam->message, "origin", json_mkstring("receiver"));
+											json_append_member(generic_webcam->message, "protocol", json_mkstring(generic_webcam->id));
+
+											pilight.broadcast(generic_webcam->id, generic_webcam->message);
+											json_delete(generic_webcam->message);
+											generic_webcam->message = NULL;
+										}
+									}
+								} else {
+									logprintf(LOG_NOTICE, "api.generic_webcam.com json has no current_observation key");
+								}
+								json_delete(jdata);
+							} else {
+								logprintf(LOG_NOTICE, "api.generic_webcam.com json could not be parsed");
+							}
+						}  else {
+							logprintf(LOG_NOTICE, "api.generic_webcam.com response was not in a valid json format");
+						}
+					} else {
+						logprintf(LOG_NOTICE, "api.generic_webcam.com response was not in a valid json format");
+					}
+				} else {
+					logprintf(LOG_NOTICE, "could not reach api.wundergrond.com");
+				}
+				if(data) {
+					sfree((void *)&data);
+				}
+				if(filename) {
+					sfree((void *)&filename);
+				}
+			wtmp = wtmp->next;
+			}
+		}
+		pthread_mutex_unlock(&mutex);				
+	}
+
+	pthread_cleanup_pop(1);
+
+	return (void *)NULL;
+	*/
 }
 
-void genWebcamInit(void) {
-	
+void generic_webcamInitDev(JsonNode *jdevice) {
+	char *output = json_stringify(jdevice, NULL);
+	JsonNode *json = json_decode(output);
+	threads_register("generic_webcam", &generic_webcamParse, (void *)json);
+	sfree((void *)&output);
+}
+
+int generic_webcamCheckValues(JsonNode *code) {
+	int interval = 900;
+	protocol_setting_get_number(generic_webcam, "interval", &interval);
+
+	if(interval < 900) {
+		return 1;
+	}
+
+	return 0;
+}
+
+void generic_webcamInit(void) {
+
 	protocol_register(&generic_webcam);
 	protocol_set_id(generic_webcam, "generic_webcam");
-	protocol_device_add(generic_webcam, "generic_webcam", "Generic web camera");
+	protocol_device_add(generic_webcam, "generic_webcam", "Generic Webcam");
 	generic_webcam->devtype = WEBCAM;
+	generic_webcam->hwtype = API;
 
-	options_add(&generic_webcam->options, 'u', "url", has_value, config_value, "^[ABCDE](3[012]?|[012][0-9]|[0-9]{1})$");
-	options_add(&generic_webcam->options, 'r', "refresh", has_value, config_value, "[0-9]");
-	options_add(&generic_webcam->options, 'i', "id", has_value, config_id, "[0-9]");
+	options_add(&generic_webcam->options, 'u', "url", has_value, config_id, "^[a-z0-9]+$");
+	protocol_setting_add_number(generic_webcam, "interval", 900);
 
-	//protocol_setting_add_number(generic_webcam, "decimals", 2);	
-	protocol_setting_add_string(generic_webcam, "url", "http://");
-	protocol_setting_add_number(generic_webcam, "refresh", 1);
-
-	generic_webcam->printHelp=&genWebcamPrintHelp;
-	generic_webcam->createCode=&genWebcamCreateCode;
+	generic_webcam->initDev=&generic_webcamInitDev;
+	generic_webcam->checkValues=&generic_webcamCheckValues;
 }
