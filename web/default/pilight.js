252e9e5ca66951a6eb771ddb9f3a825fb5721467
1000io <1000io.com@gmail.com>
2014-02-05 10:21:25 +0100

    Update pilight.js

--- web/default/pilight.js
+++ web/default/pilight.js
@@ -307,7 +307,7 @@ function createWeatherElement(sTabId, sDevId, aValues) {
 function createWattmeterElement(sTabId, sDevId, aValues) {
 	aDecimals[sTabId+'_'+sDevId] = aValues['settings']['decimals'];
 	aValues['watt'] /= Math.pow(10, aValues['settings']['decimals']).toFixed(aValues['settings']['decimals']);
-	aValues['price'] /= Math.pow(10, aValues['settings']['decimals']).toFixed(aValues['settings']['decimals']);
+	//aValues['price'] /= Math.pow(10, aValues['settings']['decimals']).toFixed(aValues['settings']['decimals']);
 	if($('#'+sTabId+'_'+sDevId+'_wattmeter').length == 0) {
 		if(bShowTabs) {
 			oTab = $('#'+sTabId).find('ul');
@@ -315,18 +315,20 @@ function createWattmeterElement(sTabId, sDevId, aValues) {
 			oTab = $('#all');
 		}
 		oTab.append($('<li class="wattmeter" id="'+sTabId+'_'+sDevId+'_wattmeter" data-icon="false">'+aValues['name']+'</li>'));
-		if(aValues['settings']['price']) {
+/*		if(aValues['settings']['price']) {
 			oTab.find('#'+sTabId+'_'+sDevId+'_wattmeter').append($('<div class="coin">€</div><div class="price" id="'+sTabId+'_'+sDevId+'_price">'+aValues['price']+'</div>'));
-		}
-		if(aValues['settings']['watt']) {
+		}*/
+		if(aValues['watt']) {
 			oTab.find('#'+sTabId+'_'+sDevId+'_wattmeter').append($('<div class="wattios">W</div><div class="watt" id="'+sTabId+'_'+sDevId+'_watt">'+aValues['watt']+'</div>'));
+			oTab.find('#'+sTabId+'_'+sDevId+'_wattmeter').append($('<div class="coin">€</div><div class="price" id="'+sTabId+'_'+sDevId+'_price">'+aValues['settings']['price']+'</div>'));
 		}
 	} else {
-		if(aValues['settings']['price']) {
+/*		if(aValues['settings']['price']) {
 			$('#'+sTabId+'_'+sDevId+'_price').text(aValues['price']);
-		}
-		if(aValues['settings']['watt']) {
+		}*/
+		if(aValues['watt']) {
 			$('#'+sTabId+'_'+sDevId+'_watt').text(aValues['watt']);
+			$('#'+sTabId+'_'+sDevId+'_price').text(aValues['price']);
 		}
 	}
 	oTab.listview();
