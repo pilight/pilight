44505e4e29b9d765bd1389ec57377f49c4f80b22
1000io <1000io.com@gmail.com>
2014-02-05 12:41:04 +0100

    Update pilight.js

--- web/default/pilight.js
+++ web/default/pilight.js
@@ -316,7 +316,7 @@ function createWattmeterElement(sTabId, sDevId, aValues) {
 		}
 		oTab.append($('<li class="wattmeter" id="'+sTabId+'_'+sDevId+'_wattmeter" data-icon="false">'+aValues['name']+'</li>'));
 		if(aValues['price']) {
-			oTab.find('#'+sTabId+'_'+sDevId+'_wattmeter').append($('<div class="coin">€</div><div class="price" id="'+sTabId+'_'+sDevId+'_price">'+(aValues['price']*aValues['watt'])+'</div>'));
+			oTab.find('#'+sTabId+'_'+sDevId+'_wattmeter').append($('<div class="coin">'+aValues['coin']+'</div><div class="price" id="'+sTabId+'_'+sDevId+'_price">'+(aValues['price']*aValues['watt'])+'</div>'));
 		}
 		if(aValues['watt']) {
 			oTab.find('#'+sTabId+'_'+sDevId+'_wattmeter').append($('<div class="wattios">W</div><div class="watt" id="'+sTabId+'_'+sDevId+'_watt">'+aValues['watt']+'</div>'));
