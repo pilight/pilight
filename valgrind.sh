7040b8d5fb4fa09242525171e5b0c92786e0047b
wo-rasp <mail_01@wheck.de>
2014-08-16 21:55:06 +0200

    Added basic _contact support (type 6) to WEB Interface (pilight.js)

--- valgrind.sh
+++ valgrind.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo valgrind --show-reachable=yes --dsymutil=yes --tool=memcheck --leak-check=full --track-origins=yes ./pilight-daemon -D
+valgrind --show-reachable=yes --dsymutil=yes --tool=memcheck --leak-check=full --track-origins=yes ./pilight-daemon -D
