ef9669ec0903fd78e9e02e44e38a3a2d47125421
CurlyMoo <curlymoo1@gmail.com>
2015-01-11 13:27:14 +0100

    Fixes in module loading

--- libs/config/hardware.c
+++ libs/config/hardware.c
@@ -363,25 +363,25 @@ void hardware_init(void) {
 		while((file = readdir(d)) != NULL) {
 			stat(file->d_name, &s);
 			/* Check if file */
-			if(S_ISREG(s.st_mode) == 0) {
+			if(S_ISREG(s.st_mode) == 1) {
 				if(strstr(file->d_name, ".so") != NULL) {
 					valid = 1;
 					memset(path, '\0', PATH_MAX);
 					sprintf(path, "%s%s", hardware_root, file->d_name);
 
-					if((handle = dso_load(path))) {
+					if((handle = dso_load(path)) != NULL) {
 						init = dso_function(handle, "init");
 						compatibility = dso_function(handle, "compatibility");
-						if(init && compatibility) {
+						if(init != NULL && compatibility != NULL ) {
 							compatibility(&module);
-							if(module.name && module.version && module.reqversion) {
+							if(module.name != NULL && module.version != NULL && module.reqversion != NULL) {
 								char ver[strlen(module.reqversion)+1];
 								strcpy(ver, module.reqversion);
 
 								if((check1 = vercmp(ver, pilight_version)) > 0) {
 									valid = 0;
 								}
-								if(check1 == 0 && module.reqcommit) {
+								if(check1 == 0 && module.reqcommit != NULL) {
 									char com[strlen(module.reqcommit)+1];
 									strcpy(com, module.reqcommit);
 									sscanf(HASH, "v%*[0-9].%*[0-9]-%[0-9]-%*[0-9a-zA-Z\n\r]", pilight_commit);
@@ -391,14 +391,14 @@ void hardware_init(void) {
 									}
 								}
 
-								if(valid) {
+								if(valid == 1) {
 									char tmp[strlen(module.name)+1];
 									strcpy(tmp, module.name);
 									hardware_remove(tmp);
 									init();
-									logprintf(LOG_DEBUG, "loaded config hardware module %s", file->d_name);
+									logprintf(LOG_DEBUG, "loaded config hardware module %s v%s", file->d_name, module.version);
 								} else {
-									if(module.reqcommit) {
+									if(module.reqcommit != NULL) {
 										logprintf(LOG_ERR, "config hardware module %s requires at least pilight v%s (commit %s)", file->d_name, module.reqversion, module.reqcommit);
 									} else {
 										logprintf(LOG_ERR, "config hardware module %s requires at least pilight v%s", file->d_name, module.reqversion);
