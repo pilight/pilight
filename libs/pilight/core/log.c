9541d200d372141e447de269c4d010fb248dd867
CurlyMoo <curlymoo1@gmail.com>
2015-03-26 09:16:35 +0100

    Let log use UTC time to prevent TZ shifts

--- libs/pilight/core/log.c
+++ libs/pilight/core/log.c
@@ -178,9 +178,9 @@ void logprintf(int prio, const char *format_str, ...) {
 	if(loglevel >= prio) {
 		gettimeofday(&tv, NULL);
 #ifdef _WIN32
-		if((localtime(&tv.tv_sec)) != 0) {
+		if((gmtime(&tv.tv_sec)) != 0) {
 #else
-		if((localtime_r(&tv.tv_sec, &tm)) != 0) {
+		if((gmtime_r(&tv.tv_sec, &tm)) != 0) {
 #endif
 			strftime(fmt, sizeof(fmt), "%b %d %H:%M:%S", &tm);
 			snprintf(buf, sizeof(buf), "%s:%03u", fmt, (unsigned int)tv.tv_usec);
