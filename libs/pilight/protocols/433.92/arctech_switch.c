1f93979d7a3aa6e6f81c83b3009bf69636b68485
CurlyMoo <curlymoo1@gmail.com>
2015-07-19 23:46:03 +0200

    Additional arctech pulse lengths

--- libs/pilight/protocols/433.92/arctech_switch.c
+++ libs/pilight/protocols/433.92/arctech_switch.c
@@ -33,7 +33,7 @@
 #define LEARN_REPEATS			40
 #define NORMAL_REPEATS		10
 #define PULSE_MULTIPLIER	4
-#define MIN_PULSE_LENGTH	274
+#define MIN_PULSE_LENGTH	250
 #define MAX_PULSE_LENGTH	320
 #define AVG_PULSE_LENGTH	315
 #define RAW_LENGTH				132
@@ -265,7 +265,7 @@ void arctechSwitchInit(void) {
 #if defined(MODULE) && !defined(_WIN32)
 void compatibility(struct module_t *module) {
 	module->name = "arctech_switch";
-	module->version = "3.2";
+	module->version = "3.3";
 	module->reqversion = "6.0";
 	module->reqcommit = "84";
 }
