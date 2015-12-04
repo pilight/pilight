1caff7e276793fcca9efcd5e8b6173ecf2eac2b5
Erwin <prutsky@users.noreply.github.com>
2015-12-04 22:26:18 +0100

    Update ge_chip.c
    Edited copyright

--- libs/pilight/protocols/433.92/ge_chip.c
+++ libs/pilight/protocols/433.92/ge_chip.c
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2014 CurlyMo
+	Copyright (C) 2015 Prutsky
 	This file is part of pilight.
 	pilight is free software: you can redistribute it and/or modify it under the
 	terms of the GNU General Public License as published by the Free Software
