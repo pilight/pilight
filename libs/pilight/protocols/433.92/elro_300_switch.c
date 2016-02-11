98ad9dba4e0f401a66c381efb3f0f3919f651c9d
cxxcoder <cxxcoder@gmx-topmail.de>
2016-02-11 10:35:53 +0100

    fix buffer overflow in elro_300_switch protocol and restore behavior from before commit f70109f8

--- libs/pilight/protocols/433.92/elro_300_switch.c
+++ libs/pilight/protocols/433.92/elro_300_switch.c
@@ -87,8 +87,12 @@ static void parseCode(void) {
 
 	for(i=0; i < elro_300_switch->rawlen; i++) {
 		if(elro_300_switch->raw[i] > (int)((double)AVG_PULSE_LENGTH*((double)PULSE_MULTIPLIER/2))) {
-			binary[x++] = 1;
-		} else {
+			if(i&1) {
+				binary[x++] = 1;
+			} else {
+				return; // even pulse lengths must be low
+			}
+		} else if(i&1) {
 			binary[x++] = 0;
 		}
 	}
