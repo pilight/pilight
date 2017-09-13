5a80e2fe7eda1e14095af4069996080935e917af
pilino1234 <pilino1234@zoho.eu>
2017-09-13 16:04:45 +0200

    Add more compilers to the Travis build matrix
    This ensures that pilight compiles on a wide variety of systems and
    configurations.

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -e
 
+# set CC to the correct compiler
+if [[ "${COMPILER}" == "gcc-6" ]]; then
+    export CC=gcc-6
+    export CXX=g++-6
+elif [[ "${COMPILER}" == "gcc-7" ]]; then
+    export CC=gcc-7
+    export CXX=g++-7
+elif [[ "${COMPILER}" == "clang-3.9" ]]; then
+    export CC=clang-3.9
+    export CXX=clang++-3.9
+elif [[ "${COMPILER}" == "clang-5.0" ]]; then
+    export CC=clang-5.0
+    export CXX=clang++-5.0
+fi
+
 if [[ ${PLATFORM} == "Unix" ]]; then
     mkdir -p build
     cd build || exit 1
