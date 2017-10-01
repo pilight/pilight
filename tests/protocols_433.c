faf56c326d50faaab5052f0c6e3d04051b6daa0a
hernae15 <eguevara@web.de>
2017-10-01 16:39:04 +0200

    Added unit test

--- tests/protocols_433.c
+++ tests/protocols_433.c
@@ -22,6 +22,7 @@
 #include "../libs/pilight/protocols/433.92/arctech_dimmer.h"
 #include "../libs/pilight/protocols/433.92/arctech_dusk.h"
 #include "../libs/pilight/protocols/433.92/livolo_switch.h"
+#include "../libs/pilight/protocols/433.92/logilink_switch2.h"
 
 #include "alltests.h"
 
@@ -45,6 +46,7 @@ const struct test_t {
 #include "protocols/arctech_dimmer.h"
 #include "protocols/arctech_dusk.h"
 #include "protocols/livolo_switch.h"
+#include "protocols/logilink_switch2.h"
 
 static const struct test_t tests[] = {
 	{ &alectoWS1700Init, &alecto_ws1700, alecto_ws1700_tests, &alecto_ws1700_nrtests },
@@ -53,6 +55,7 @@ static const struct test_t tests[] = {
 	{ &arctechDimmerInit, &arctech_dimmer, arctech_dimmer_tests, &arctech_dimmer_nrtests },
 	{ &arctechDuskInit, &arctech_dusk, arctech_dusk_tests, &arctech_dusk_nrtests },
 	{ &livoloSwitchInit, &livolo_switch, livolo_switch_tests, &livolo_switch_nrtests },
+	{ &logilinkSwitch2Init, &logilink_switch2, logilink_switch2_tests, &logilink_switch2_nrtests },
 };
 
 static void test_protocols_433(CuTest *tc) {
