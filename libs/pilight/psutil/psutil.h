427e5e97adee9d450ac212ab053550770477f635
pilino1234 <pilino1234@zoho.eu>
2017-12-06 11:27:27 +0100

    Fix segfault in isrunning() on encountering long process name
    Running the slack or spotify desktop apps while running the unittests revealed
    that the process name was too long for the function to store in a char array
    of size 255. Fixed by increasing the size of the buffer to 1024, and add the
    bufsize as an argument to psutil_proc_name() to allocate a dynamic buffer size.

--- libs/pilight/psutil/psutil.h
+++ libs/pilight/psutil/psutil.h
@@ -4,6 +4,6 @@
 
 unsigned long psutil_max_pid(void);
 int psutil_proc_exe(const pid_t pid, char **name);
-int psutil_proc_name(const pid_t pid, char **name);
+int psutil_proc_name(const pid_t pid, char **name, size_t bufsize);
 int psutil_cpu_count_phys(void);
 size_t psutil_get_cmd_args(pid_t pid, char **args, size_t size);
