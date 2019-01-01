b3ba9505219630201d2a70f171503e82d2f07eac
mwka <matthias@walliczek.de>
2019-01-01 17:01:39 +0100

    add kerui protocol

---
+++ tests/protocols/kerui_d026.h
@@ -0,0 +1,9 @@
+static const int kerui_d026_nrtests = 1;
+static const struct raw_t kerui_d026_tests[] = {
+	{
+		"1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 8840",
+		"{\"unitcode\":0,\"state\":\"opened\"}",
+		NULL,
+		0
+	}
+};
