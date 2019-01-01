4950b25055646fef7583ed38b3a0de68817b452d
mwka <matthias@walliczek.de>
2019-01-01 19:47:40 +0100

    add more test cases

--- tests/protocols/kerui_d026.h
+++ tests/protocols/kerui_d026.h
@@ -1,8 +1,14 @@
-static const int kerui_d026_nrtests = 1;
+static const int kerui_d026_nrtests = 2;
 static const struct raw_t kerui_d026_tests[] = {
 	{
 		"1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 8840",
 		"{\"unitcode\":0,\"state\":\"opened\"}",
+		NULL,
+		0
+	},
+	{
+		"1000 1000 1000 1000 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1000 1 1 8840",
+		"{\"unitcode\":3,\"state\":\"closed\"}",
 		NULL,
 		0
 	}
