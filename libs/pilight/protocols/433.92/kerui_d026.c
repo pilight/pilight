ae0d471cd04f690ff0487276b893d188330f0ae5
mwka <matthias@walliczek.de>
2019-01-11 06:17:29 +0100

    fixed indenting

--- libs/pilight/protocols/433.92/kerui_d026.c
+++ libs/pilight/protocols/433.92/kerui_d026.c
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2017 - 2019 by CurlyMo, Meloen, zeltom, mwka
+  Copyright (C) 2017 - 2019 by CurlyMo, Meloen, zeltom, mwka
 
   This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. If a copy of the MPL was not distributed with this
