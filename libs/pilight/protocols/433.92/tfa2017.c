8c629fc72b1782d5152b21c22cc1954a9b923be5
Michael Behrisch <oss@behrisch.de>
2019-01-19 13:45:42 +0100

    fixing license and typo

--- libs/pilight/protocols/433.92/tfa2017.c
+++ libs/pilight/protocols/433.92/tfa2017.c
@@ -1,19 +1,9 @@
 /*
 	Copyright (C) 2014, 2018 CurlyMo & DonBernos & Michael Behrisch
 
-	This file is part of pilight.
-
-	pilight is free software: you can redistribute it and/or modify it under the
-	terms of the GNU General Public License as published by the Free Software
-	Foundation, either version 3 of the License, or (at your option) any later
-	version.
-
-	pilight is distributed in the hope that it will be useful, but WITHOUT ANY
-	WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-	A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with pilight. If not, see	<http://www.gnu.org/licenses/>
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 #include <stdio.h>
@@ -209,7 +199,7 @@ __attribute__((weak))
 void tfa2017Init(void) {
 	protocol_register(&tfa2017);
 	protocol_set_id(tfa2017, "tfa2017");
-	protocol_device_add(tfa2017, "tfa2017", "TFA 30.X Temp Hum Sensor Revision 08/2017");
+	protocol_device_add(tfa2017, "tfa2017", "TFA 30.X Temp Hum Sensor Revision 09/2017");
 	tfa2017->devtype = WEATHER;
 	tfa2017->hwtype = RF433;
 	tfa2017->minrawlen = MIN_RAW_LENGTH;
