56b182b782bbad20c27b2e0b2134e291cb3b4a4a
mwka <35632862+mwka@users.noreply.github.com>
2019-01-24 19:27:44 +0100

    KERUI protocol with unittest, in addition to PR #421 (#423)

--- tests/protocols_433.c
+++ tests/protocols_433.c
@@ -22,6 +22,7 @@
 #include "../libs/pilight/protocols/433.92/arctech_dimmer.h"
 #include "../libs/pilight/protocols/433.92/arctech_dusk.h"
 #include "../libs/pilight/protocols/433.92/livolo_switch.h"
+#include "../libs/pilight/protocols/433.92/kerui_d026.h"
 #include "../libs/pilight/protocols/433.92/tfa2017.h"
 
 #include "alltests.h"
@@ -46,6 +47,7 @@ const struct test_t {
 #include "protocols/arctech_dimmer.h"
 #include "protocols/arctech_dusk.h"
 #include "protocols/livolo_switch.h"
+#include "protocols/kerui_d026.h"
 #include "protocols/tfa2017.h"
 
 static const struct test_t tests[] = {
@@ -55,6 +57,7 @@ static const struct test_t tests[] = {
 	{ &arctechDimmerInit, &arctech_dimmer, arctech_dimmer_tests, &arctech_dimmer_nrtests },
 	{ &arctechDuskInit, &arctech_dusk, arctech_dusk_tests, &arctech_dusk_nrtests },
 	{ &livoloSwitchInit, &livolo_switch, livolo_switch_tests, &livolo_switch_nrtests },
+	{ &keruiD026Init, &kerui_D026, kerui_d026_tests, &kerui_d026_nrtests },
 	{ &tfa2017Init, &tfa2017, tfa2017_tests, &tfa2017_nrtests },
 };
 
