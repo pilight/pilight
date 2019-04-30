f6a8537d3a39a9b1f1f1a8240d5c3d5fee708971
CurlyMoo <curlymoo1@gmail.com>
2019-04-30 21:39:27 +0200

    Added hardware header and files

--- libs/pilight/config/hardware.h
+++ libs/pilight/config/hardware.h
@@ -1,14 +1,22 @@
 /*
-	Copyright (C) 2013 - 2016 CurlyMo
+  Copyright (C) CurlyMo
 
   This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. If a copy of the MPL was not distributed with this
   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-#ifndef _CONFIG_HARDWARE_H_
-#define _CONFIG_HARDWARE_H_
+#ifndef _LUA_CONFIG_HARDWARE_H_
+#define _LUA_CONFIG_HARDWARE_H_
 
-#include "../lua_c/lua.h"
+#include "../lua.h"
+
+typedef struct plua_hardware_t {
+	char *name;
+	struct plua_metatable_t *table;
+	struct hardware_t *ptr;
+} plua_hardware_t;
+
+int plua_config_hardware(lua_State *L);
 
 #endif
