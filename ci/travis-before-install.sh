9f2d2c8618e96d69cf7ea5c1b20c0b6842818c76
CurlyMoo <curlymoo1@gmail.com>
2019-05-15 20:05:29 +0200

    Download libmbedtls from own repo

--- ci/travis-before-install.sh
+++ ci/travis-before-install.sh
@@ -3,13 +3,14 @@
 if [[ $PLATFORM == "Unix" ]]; then
     curl https://apt.pilight.org/pilight.key | sudo apt-key add - && echo "deb http://apt.pilight.org/ stable main" | sudo tee -a /etc/apt/sources.list
     sudo apt-get update
+    sudo apt-cache search libmbedtls
     sudo apt-get install libwiringx-dev libwiringx libpcap-dev libunwind8-dev lcov libluajit-5.1-2 libluajit-5.1-dev -y
 
     mkdir depends && cd depends
-    wget http://security.ubuntu.com/ubuntu/pool/universe/m/mbedtls/libmbedcrypto1_2.8.0-1_amd64.deb
-    wget http://security.ubuntu.com/ubuntu/pool/universe/m/mbedtls/libmbedtls10_2.8.0-1_amd64.deb
-    wget http://security.ubuntu.com/ubuntu/pool/universe/m/mbedtls/libmbedx509-0_2.8.0-1_amd64.deb
-    wget http://security.ubuntu.com/ubuntu/pool/universe/m/mbedtls/libmbedtls-dev_2.8.0-1_amd64.deb
+    wget http://apt.pilight.org/pool/stable/main/l/libmbedcrypto0/libmbedcrypto0_2.5.1-1ubuntu1_amd64.deb
+    wget http://apt.pilight.org/pool/stable/main/l/libmbedtls-dev/libmbedtls-dev_2.5.1-1ubuntu1_amd64.deb
+    wget http://apt.pilight.org/pool/stable/main/l/libmbedx509-0/libmbedx509-0_2.5.1-1ubuntu1_amd64.deb
+    wget http://apt.pilight.org/pool/stable/main/l/libmbedtls10/libmbedtls10_2.5.1-1ubuntu1_amd64.deb
     sudo dpkg -i *.deb
     cd ..
 fi
