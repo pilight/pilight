52d597157859b15c2d7335a281bc47090f3786e7
CurlyMoo <curlymoo1@gmail.com>
2020-01-15 21:30:58 +0100

    Allow unauthenticated package install on travis

--- ci/travis-before-install.sh
+++ ci/travis-before-install.sh
@@ -4,7 +4,7 @@ if [[ $PLATFORM == "Unix" ]]; then
     curl https://apt.pilight.org/pilight.key | sudo apt-key add - && echo "deb http://apt.pilight.org/ stable main" | sudo tee -a /etc/apt/sources.list
     sudo apt-get update
     sudo apt-cache search libmbedtls
-    sudo apt-get install libwiringx-dev libwiringx libpcap-dev libunwind8-dev lcov libluajit-5.1-2 libluajit-5.1-dev -y
+    sudo apt-get install libwiringx-dev libwiringx libpcap-dev libunwind8-dev lcov libluajit-5.1-2 libluajit-5.1-dev -y --allow-unauthenticated
 
     mkdir depends && cd depends
     wget http://apt.pilight.org/pool/stable/main/l/libmbedcrypto0/libmbedcrypto0_2.5.1-1ubuntu1_amd64.deb
