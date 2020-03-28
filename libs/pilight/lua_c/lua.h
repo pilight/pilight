3dd52c187a5ce5074c7acda917f757b3dd35e7e8
LeoCal <25389602+LeoCal@users.noreply.github.com>
2020-03-28 13:21:44 +0100

    Adding support for Smartwares switch

--- libs/pilight/lua_c/lua.h
+++ libs/pilight/lua_c/lua.h
@@ -11,9 +11,9 @@
 
 #include <setjmp.h>
 
-#include <luajit-2.0/lua.h>
-#include <luajit-2.0/lualib.h>
-#include <luajit-2.0/lauxlib.h>
+#include <luajit-2.1/lua.h>
+#include <luajit-2.1/lualib.h>
+#include <luajit-2.1/lauxlib.h>
 
 // #include "ctrace.h"
 
