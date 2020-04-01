94ecd3119b9a1c6a1a927e8accea2e9027903542
Tom van der Geer <t.vandergeer@sping.nl>
2020-04-01 23:28:57 +0200

    Added unit tests for APIR-2150 support in rewrite branch (#452)
    Now supports KaKu/CoCo APIR-2150 motion sensors

--- tests/protocols_433.c
+++ tests/protocols_433.c
@@ -21,6 +21,7 @@
 #include "../libs/pilight/protocols/433.92/arctech_contact.h"
 #include "../libs/pilight/protocols/433.92/arctech_dimmer.h"
 #include "../libs/pilight/protocols/433.92/arctech_dusk.h"
+#include "../libs/pilight/protocols/433.92/arctech_motion.h"
 #include "../libs/pilight/protocols/433.92/livolo_switch.h"
 #include "../libs/pilight/protocols/433.92/iwds07.h"
 #include "../libs/pilight/protocols/433.92/kerui_d026.h"
@@ -47,6 +48,7 @@ const struct test_t {
 #include "protocols/arctech_contact.h"
 #include "protocols/arctech_dimmer.h"
 #include "protocols/arctech_dusk.h"
+#include "protocols/arctech_motion.h"
 #include "protocols/livolo_switch.h"
 #include "protocols/iwds07.h"
 #include "protocols/kerui_d026.h"
@@ -56,6 +58,7 @@ static const struct test_t tests[] = {
 	{ &alectoWS1700Init, &alecto_ws1700, alecto_ws1700_tests, &alecto_ws1700_nrtests },
 	{ &alectoWX500Init, &alecto_wx500, alecto_wx500_tests, &alecto_wx500_nrtests },
 	{ &arctechContactInit, &arctech_contact, arctech_contact_tests, &arctech_contact_nrtests },
+	{ &arctechMotionInit, &arctech_motion, arctech_motion_tests, &arctech_motion_nrtests },
 	{ &iwds07Init, &iwds07, iwds07_tests, &iwds07_nrtests },
 	{ &arctechDimmerInit, &arctech_dimmer, arctech_dimmer_tests, &arctech_dimmer_nrtests },
 	{ &arctechDuskInit, &arctech_dusk, arctech_dusk_tests, &arctech_dusk_nrtests },
