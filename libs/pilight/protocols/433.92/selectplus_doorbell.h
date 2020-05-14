5e71a1a024b87adb95ddee805f0179ab4495ffd8
Wouter van der Put <wouter@wouterendieuwke.be>
2020-05-14 22:19:25 +0200

    Adding protocol SelectPlus doorbell

---
+++ libs/pilight/protocols/433.92/selectplus_doorbell.h
@@ -0,0 +1,17 @@
+/*
+	Copyright (C) 2020 woutput
+	This file is part of pilight.
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#ifndef _PROTOCOL_SELECTPLUS_DOORBELL_H_
+#define _PROTOCOL_SELECTPLUS_DOORBELL_H_
+
+#include "../protocol.h"
+
+struct protocol_t *selectplus_doorbell;
+void selectplusDoorbellInit(void);
+
+#endif
