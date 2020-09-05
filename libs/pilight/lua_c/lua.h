d7dfff165e564ccb3e41df9ae6726313a46c0254
CurlyMoo <curlymoo1@gmail.com>
2020-09-05 10:59:02 +0200

    Automatically find lua header version

--- libs/pilight/lua_c/lua.h
+++ libs/pilight/lua_c/lua.h
@@ -11,9 +11,9 @@
 
 #include <setjmp.h>
 
-#include <luajit-2.0/lua.h>
-#include <luajit-2.0/lualib.h>
-#include <luajit-2.0/lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
 
 // #include "ctrace.h"
 
