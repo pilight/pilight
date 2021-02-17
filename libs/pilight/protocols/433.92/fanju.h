63e8fb566e089e8266893e8d1ef165cf2b813152
blokkendoos <jvo@chaosgeordend.nl>
2021-02-17 15:51:42 +0100

    Add Fanju protocol

---
+++ libs/pilight/protocols/433.92/fanju.h
@@ -0,0 +1,18 @@
+/*
+  Copyright (C) 2021 CurlyMo 
+  Copyright (C) 2021 Johan van Oostrum 
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#ifndef _PROTOCOL_FANJU_H_
+#define _PROTOCOL_FANJU_H_
+
+#include "../protocol.h"
+
+struct protocol_t *fanju;
+void fanjuInit(void);
+
+#endif
