182a8eb0f2156f10fad827eed79dc6b5d14e664d
Paul <steadramon@local>
2022-06-05 18:19:52 +0100

    ID up to 999999

--- libs/pilight/protocols/433.92/hs2441pt.c
+++ libs/pilight/protocols/433.92/hs2441pt.c
@@ -83,7 +83,7 @@ void hs2241ptInit(void) {
 	hs2241pt->maxgaplen = MAX_PULSE_LENGTH*PULSE_DIV;
 	hs2241pt->mingaplen = MIN_PULSE_LENGTH*PULSE_DIV;
 
-	options_add(&hs2241pt->options, "id", "id", OPTION_HAS_VALUE, DEVICES_ID, JSON_NUMBER, NULL, "^[0-9]{1,5}");
+	options_add(&hs2241pt->options, "id", "id", OPTION_HAS_VALUE, DEVICES_ID, JSON_NUMBER, NULL, "^[0-9]{1,6}");
 	options_add(&hs2241pt->options, "s", "state", OPTION_NO_VALUE, DEVICES_STATE, JSON_NUMBER, NULL, "^([0-9]|1[0-5])$");
 
 	hs2241pt->parseCode=&parseCode;
